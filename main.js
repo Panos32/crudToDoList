252fa8027d67093fcec551aa38cd904f2be99ce1
Panos32 <97842668+Panos32@users.noreply.github.com>
2023-06-13 21:56:56 +0300

    js_edit_delete+mistakes

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ let textInput = document.getElementById("textInput");
 let msg = document.getElementById("msg");
 let dateInput = document.getElementById("dateInput");
 let textArea = document.getElementById("textArea");
+let tasks = document.getElementById("tasks");
+let add = document.getElementById("add");
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -15,7 +17,14 @@ let formValidation = () =>{
        msg.innerHTML = "Task cannot be blank" 
     } else {
         console.log("success");
-        msg.innerHTML = "" 
+        msg.innerHTML = "" ;
+        acceptData();
+        add.setAttribute("data-bs-dismiss","modal");
+        add.click();
+
+        (()=>{
+            add.setAttribute("data-bs-dismiss","");
+        })()
     }
 };
 
@@ -26,6 +35,41 @@ let acceptData = () => {
     data["date"] = dateInput.value;
     data["description"] = textArea.value;
 
-    colnsole.log(data);
+    createTasks();
+};
+
+let createTasks = () => {
+    tasks.innerHTML += `
+    <div>
+        <span class="fw-bold">${data.text}</span>
+        <span class="small text-secondary">${data.date}</span>
+        <p>${data.description}</p>
+
+        <span class="options">
+        <i onClick = "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+        <i onClick = "deleteTask(this)" class="fas fa-trash-alt"></i>
+        </span>
+    </div> `;
+
+    resetForm();
+};
+
+
+let deleteTask = (e) =>{
+    e.parentElement.parentElement.remove();
+};
+
+let editTask = (e) => {
+   let selectedTask = e.parentElement.parentElement;
+
+   textInput.value = selectedTask.children[o].innerHTML;
+   dateInput.value = selectedTask.children[1].innerHTML;
+   textArea.value = selectedTask.children[2].innerHTML;
+};
+
+let resetForm = () =>{
+    textInput.value = "";
+    dateInput.value = "";
+    textArea.value = "";
 };
 
